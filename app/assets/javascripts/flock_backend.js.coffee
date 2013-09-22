306b3b98db05e3047e71ce70aa4482a17812c92e
Adam Waxman <awaxman11@gmail.com>
2013-09-22 16:18:49 -0400

    Initial commit

---
+++ app/assets/javascripts/flock_backend.js.coffee
@@ -0,0 +1,9 @@
+window.FlockBackend =
+  Models: {}
+  Collections: {}
+  Views: {}
+  Routers: {}
+  initialize: -> alert 'Hello from Backbone!'
+
+$(document).ready ->
+  FlockBackend.initialize()
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FlockBackend::Application.config.secret_token = '02f1eda1e1f71064b95e4218a2889c797e40d7adb282785e328a0f52ebdf905164aa2a71b4abdd9c6a99484ce576d001f007eddc22e88466ebad08f4f41191e3'
